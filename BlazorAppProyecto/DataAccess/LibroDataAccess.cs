a1109f14ea16be1ceddafeab5a965c08752d502d
jcamilogh <jcamilogh@outlook.com>
2020-01-24 15:26:51 -0500

    Add project files.

---
+++ BlazorAppProyecto/DataAccess/LibroDataAccess.cs
@@ -0,0 +1,57 @@
+﻿using BlazorAppProyecto.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAppProyecto.DataAccess
+{
+	public class LibroDataAccess
+	{
+		public List<LibroModel> Libros { get; set; } = new List<LibroModel>();
+
+		public LibroDataAccess()
+		{
+			Libros.Add(new LibroModel {
+				Id = 1,
+				Titulo = "Cien años de soledad",
+				Precio = 20,
+				Cantidad = 15
+			});
+
+			Libros.Add(new LibroModel
+			{
+				Id = 2,
+				Titulo = "El Quijote",
+				Precio = 23,
+				Cantidad = 25
+			});
+
+			Libros.Add(new LibroModel
+			{
+				Id = 3,
+				Titulo = "La Rayuela",
+				Precio = 30,
+				Cantidad = 10
+			});
+
+			Libros.Add(new LibroModel
+			{
+				Id = 4,
+				Titulo = "Crimen y Castigo",
+				Precio = 60,
+				Cantidad = 10
+			});
+		}
+
+		public List<LibroModel> ObtenerLibros()
+		{
+			return Libros;
+		}
+
+		public void GuardarLibro(LibroModel libro)
+		{
+			Libros.Add(libro);
+		}
+	}
+}
---
+++ BlazorAppProyecto/Modelos/LibroModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAppProyecto.Modelos
+{
+	public class LibroModel
+	{
+		[Required]
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(50, ErrorMessage = "Titulo muy largo.")]
+		public string Titulo { get; set; }
+
+		[Required]
+		public decimal Precio { get; set; }
+
+		[Required]
+		public int Cantidad { get; set; }
+	}
+}
